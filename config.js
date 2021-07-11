be3da8f103e183075be9138dc80374ee26dac9d0
tranvimt02 <87269781+tranvimt02@users.noreply.github.com>
2021-07-11 21:09:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const CONFIG = {
     introDesc: `anh thích tất cả nngày trong tuần trừ chủ nhật ,vì chủ nhật hong có THU  `,
         
     btnIntro: 'hihi',
-    title: 'ngủ ngoan nhé 😙',
-    desc: 'nhớ mơ thấy tui nka  ',
-    btnYes: 'iu em',
-    btnNo: 'iu em :3',
+    title: '😙',
+    desc: 'minthu co iu meo meo khom  ',
+    btnYes: 'meo cung iu mthu ',
+    btnNo: 'kh sao đợi tới lúc mthu iu cũng đc :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em biết không  <3',
-    btnReply: 'vì em là người duy nhất trong 7 tỉ người lấy được trái tym anh <3',
+    btnReply: 'next',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'công chúa ngủ ngonnnnn',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
