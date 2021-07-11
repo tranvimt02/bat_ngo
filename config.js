286bad94685a5d7a4626aaaca572cf9fa371748c
tranvimt02 <87269781+tranvimt02@users.noreply.github.com>
2021-07-11 21:02:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,14 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Bae à!',
+    introDesc: `anh thích tất cả nngày trong tuần trừ chủ nhật ,vì chủ nhật hong có THU  `,
+        
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'ngủ ngoan nhé 😙',
+    desc: 'nhớ mơ thấy tui nka  ',
+    btnYes: 'iu em',
+    btnNo: 'iu em :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em biết không  <3',
+    btnReply: 'vì em là người duy nhất trong 7 tỉ người lấy được trái tym anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
